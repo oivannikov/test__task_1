87d74322a532c5180b95a43e0d661f4458aab682
oivannikov <alex0502ivannikov@gmail.com>
2020-09-29 17:35:23 +0300

    Updates

---
+++ precache-manifest.6b262774b443c48938defffea7f44d7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ee42d0e049a7685b98dca68f54e652",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "522d6e8aa074b7f9675b",
+    "url": "/test/static/css/2.ad17f843.chunk.css"
+  },
+  {
+    "revision": "f1613d8b5c0a7fb99cde",
+    "url": "/test/static/css/main.aaf580a9.chunk.css"
+  },
+  {
+    "revision": "522d6e8aa074b7f9675b",
+    "url": "/test/static/js/2.e6618478.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test/static/js/2.e6618478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1613d8b5c0a7fb99cde",
+    "url": "/test/static/js/main.20df5f70.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/test/static/media/homeContent.37a75b54.jpg"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/test/static/media/logo.b1a38376.svg"
+  }
+]);
